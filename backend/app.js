1a8174288e2b7c6ea6e778d02850ccd16850db0b
Jaswant Singh <jaswant9461412627@gmail.com>
2024-06-09 20:57:20 +0530

    add demo route

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.get('/', (req, res) => {
+    res.send('Hello, sunil!');
+  });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
